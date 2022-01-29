8020e8463555e234398baed578284da1f1c4fc90
DonaLd-D <1376825153@qq.com>
2022-01-29 16:10:47 +0800

    update

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/junhui/firstGoProj/myCalc"
 )
 
 func main(){
 	fmt.Println("Hello World")
+	fmt.Println(mathClass.Add(1,2))
+	fmt.Println(mathClass.Sub(1,2))
 }
